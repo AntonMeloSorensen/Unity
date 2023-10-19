fb77e5d1a39778dcc947622778eb0a39daacb754
Anton Melo Sorensen <antonmelosorensen@gmail.com>
2023-10-19 09:28:15 +0200

    Printed to the log

--- Anton experimenting/Assets/PrintToConsole.cs
+++ Anton experimenting/Assets/PrintToConsole.cs
@@ -7,7 +7,7 @@ public class PrintToConsole : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Im printing to the console");
+        Debug.Log("I was just driving down the road when a tank aproached and shot me and another car down. why didnt he stay in the tank line?");
     }
 
     // Update is called once per frame
