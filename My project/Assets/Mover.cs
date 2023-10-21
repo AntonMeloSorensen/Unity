aa0ac24bc5b887838878bc4e800bbb0160d2b25d
Anton Melo Sorensen <antonmelosorensen@gmail.com>
2023-10-21 13:08:04 +0200

    Added Basic frame-Independent Movement

--- My project/Assets/Mover.cs
+++ My project/Assets/Mover.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-
-     [SerializeField]float xValue = 0;
-     [SerializeField]float yValue = 0;
-     [SerializeField]float zValue = 0.01f;
+     [SerializeField]float moveSpeed = 15f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +14,9 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(xValue,yValue,zValue);
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        
+        transform.Translate(xValue,0,zValue);
     }
 }
